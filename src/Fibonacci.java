584e82d2edc17b4cd000ae1cbceafe6a6d65b75f
Mahmoud Amin <dr.mahmoud39@gmail.com>
2021-05-31 21:16:36 +0200

    Implement Fibonacci

---
+++ src/Fibonacci.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        int nthNumber = Integer.parseInt(input);
+
+        int recursiveApproachOutput = getFibonacciNumberRecursively(nthNumber);
+        System.out.println("Recursively: " + recursiveApproachOutput);
+
+        int iterativeApproachOutput = getFibonacciNumberIteratively(nthNumber);
+        System.out.println("Iteratively: " + iterativeApproachOutput);
+    }
+
+    private static int getFibonacciNumberRecursively(int index) {
+        if (index <= 0) {
+            return 0;
+        } else if (index == 1) {
+            return 1;
+        } else {
+            return getFibonacciNumberRecursively(index - 1) + getFibonacciNumberRecursively(index - 2);
+        }
+    }
+
+    public static int getFibonacciNumberIteratively(int index) {
+        int previousPreviousNumber, previousNumber = 0, currentNumber = 1;
+        for (int i = 1; i < index; i++) {
+            previousPreviousNumber = previousNumber;
+            previousNumber = currentNumber;
+            currentNumber = previousPreviousNumber + previousNumber;
+        }
+        return currentNumber;
+    }
+}
